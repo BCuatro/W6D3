10aeddf4931454273407233b8983f87fa009aeac
Eric Balfour <egbalfour@gmail.com>
2022-05-04 15:37:49 -0400

    create tables

---
+++ first_routes_and__controllers/db/migrate/20220504192732_art_share.rb
@@ -0,0 +1,26 @@
+class ArtShare < ActiveRecord::Migration[5.2]
+  def change
+
+    drop_table :users
+    create_table :users do |t|
+      t.string :username, null: false, unique: true
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+
+    create_table :artworks do |t|
+      t.string :title, null: false
+      t.string :image_url, null: false
+      t.integer :artist_id, null: false
+    end
+    add_index :artworks, [:title, :artist_id], unique: true
+    add_index :artworks, :artist_id
+
+    create_table :artwork_shares do |t|
+      t.integer :artwork_id, null: false
+      t.integer :viewer_id, null: false 
+    end
+    add_index :artwork_shares, [:artwork_id, :viewer_id], unique: true
+  end
+
+end
